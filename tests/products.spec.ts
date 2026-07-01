11d5ef8f1788e0f191bb55c68c5d965735f75836
Kyrylo Diachenko <sausmato@gmail.com>
2026-07-01 15:25:10 +0300

    Add data for products (test-data/products.json)
    Add test for adding different items

--- tests/products.spec.ts
+++ tests/products.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 import { ApiClient } from '../helpers/apiClient';
 import { ENDPOINTS } from '../helpers/endpoints';
 import { Product, ProductsResponse } from '../helpers/types';
-
+import productData from '../test-data/products.json'
 
 test('Length of all products is greater than 0', async({ request }) => {
     const client = new ApiClient(request);
@@ -29,3 +29,16 @@ test('Non-existant product', async({ request }) => {
 
     expect(response.status()).toBe(404);
 });
+
+for (const {title, category, tags} of productData) {
+    test(`Adding new product ${title}`, async({ request }) => {
+        const client = new ApiClient(request);
+        const response = await client.post(ENDPOINTS.products.add, {'title': title, 'category': category, 'tags': tags});
+
+        expect(response.status()).toBe(201);
+        const body = await response.json() as Product;    
+        expect(body.id).toBeDefined();
+        expect(typeof body.id).toBe('number');
+        expect(body.title).toContain(title);
+    });
+}
