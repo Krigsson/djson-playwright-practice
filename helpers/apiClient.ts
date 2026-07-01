40bce1a6684a75b69c7e3f473bd646390baefcb9
Kyrylo Diachenko <sausmato@gmail.com>
2026-07-01 15:33:38 +0300

    Remove redundant variables

--- helpers/apiClient.ts
+++ helpers/apiClient.ts
@@ -4,12 +4,10 @@ export class ApiClient {
     constructor(private request: APIRequestContext) {}
 
     async get(endpoint: string): Promise<APIResponse> {
-        const response = await this.request.get(endpoint);
         return await this.request.get(endpoint);
     }
 
     async post(endpoint: string, body: object): Promise<APIResponse> {
-        const response = await this.request.post(endpoint, { data: body });
         return await this.request.post(endpoint, {data: body});
     }
 }
