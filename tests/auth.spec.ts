eef7832d0a67f3cd7172fc0d64f9d23dfa6a41c7
Kyrylo Diachenko <sausmato@gmail.com>
2026-07-02 17:14:26 +0300

    Add incorrect login data (invalid-users.json)
    Add simple test to check incorrect login attempt

--- tests/auth.spec.ts
+++ tests/auth.spec.ts
@@ -2,10 +2,11 @@ import { test, expect } from '@playwright/test';
 import { ApiClient } from '../helpers/apiClient';
 import { ENDPOINTS } from '../helpers/endpoints';
 import { User } from '../helpers/types';
-import loginData from '../test-data/users.json'
+import correctLoginData from '../test-data/users/correct-users.json';
+import invalidLoginData from '../test-data/users/invalid-users.json';
 
-for (const { username, password } of loginData) {
-    test(`Test login for {${username}} : {${password}} set`, async ({ request }) => {
+for (const { username, password } of correctLoginData) {
+    test(`Test login for valid {${username}} : {${password}} set`, async ({ request }) => {
         const client = new ApiClient(request);
         const response = await client.post(ENDPOINTS.auth.login, { 'username': username, 'password': password });
 
@@ -14,5 +15,17 @@ for (const { username, password } of loginData) {
         expect(body.username).toContain(username);
         expect(body.id).toBeDefined();
         expect(body.email).toContain('dummyjson');
+    });
+}
+
+for (const { username, password, errorMessage } of invalidLoginData) {
+    test(`Test login for invalid {${username}} : {${password}} set`, async ({ request }) => {
+        const client = new ApiClient(request);
+        const response = await client.post(ENDPOINTS.auth.login, { 'username': username, 'password': password});
+
+        expect(response.status()).toBe(400);
+        const body = await response.json();
+        expect(body["message"]).toBeDefined();
+        expect(body["message"]).toContain(errorMessage);
     });
 }
