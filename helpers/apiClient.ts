868a01249ce292ac9fbed264d824ea18b722d411
Kyrylo Diachenko <sausmato@gmail.com>
2026-07-02 17:51:17 +0300

    Update ApiClient to receive headers and body, make them both optional

--- helpers/apiClient.ts
+++ helpers/apiClient.ts
@@ -3,12 +3,12 @@ import { APIRequestContext, APIResponse } from '@playwright/test';
 export class ApiClient {
     constructor(private request: APIRequestContext) {}
 
-    async get(endpoint: string): Promise<APIResponse> {
-        return await this.request.get(endpoint);
+    async get(endpoint: string, options?: {body?: object, headers?: Record<string, string>}): Promise<APIResponse> {
+        return await this.request.get(endpoint, { data: options?.body, headers: options?.headers});
     }
 
-    async post(endpoint: string, body: object): Promise<APIResponse> {
-        return await this.request.post(endpoint, { data: body });
+    async post(endpoint: string, options?: { body?: object, headers?: Record<string, string>}): Promise<APIResponse> {
+        return await this.request.post(endpoint, { data: options?.body, headers: options?.headers });
     }
 
     async delete(endpoint: string): Promise<APIResponse> {
